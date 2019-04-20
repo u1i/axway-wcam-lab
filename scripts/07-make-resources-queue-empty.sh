604c72271ba983a2d91dfe676c254b8e2a709990
Uli Hitzel <uli.hitzel@gmail.com>
2019-04-20 05:26:01 +0000

    changed

---
+++ scripts/07-make-resources-queue-empty.sh
@@ -0,0 +1,18 @@
+source b9y.conf
+
+# Get b9y token
+
+token=$(curl -s -X POST \
+  "$b9y_host/auth/token?raw" \
+  -H 'Cache-Control: no-cache' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "username": "'''$b9y_user'''",
+  "password": "'''$b9y_password'''"
+}')
+
+
+for x in $(seq 200)
+do
+	curl $b9y_host/lists/stuff -H "Authorization: Bearer $token" 
+done
